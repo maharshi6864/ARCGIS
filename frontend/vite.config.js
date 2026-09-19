bbed0e9c9f6cebce0f1e64446c05bc3b94498310
maharshi <maharshipatel@Maharshis-MacBook-Pro.local>
2026-09-20 01:24:53 +0530

    Changed host to anywhere for frontend.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,10 +6,10 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
-    host: '127.0.0.1',
+    host: '0.0.0.0',
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:8080',
+        target: 'http://127.0.0.1:8001',
         changeOrigin: true,
       }
     }
